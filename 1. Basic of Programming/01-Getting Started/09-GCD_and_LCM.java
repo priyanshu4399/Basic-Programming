f95a527add782ed7ae4d903113fcf6fe7e5cc0ea
Priyanshu <48436226+priyanshu4399@users.noreply.github.com>
2021-07-09 14:17:39 +0530

    remove comment
    remove comment

--- 1. Basic of Programming/01-Getting Started/09-GCD_and_LCM.java
+++ 1. Basic of Programming/01-Getting Started/09-GCD_and_LCM.java
@@ -1,7 +1,7 @@
 /*
 
 Gcd And Lcm
-Easy  Prev   Next
+
 1. You are required to print the Greatest Common Divisor (GCD) of two numbers.
 2. You are also required to print the Lowest Common Multiple (LCM) of the same numbers.
 3. Take input "num1" and "num2" as the two numbers.
